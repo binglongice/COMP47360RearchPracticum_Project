d216e9fca4c966a8e86b6b0d36a819c4bef110bc
larkincol <123472310+larkincol@users.noreply.github.com>
2023-06-29 14:38:48 +0100

    Update models.py

--- yelp_integration/yelp_api/models.py
+++ yelp_integration/yelp_api/models.py
@@ -6,3 +6,6 @@ class Cafe(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=1)
     latitude = models.FloatField()
     longitude = models.FloatField()
+
+    def __str__(self):
+        return self.name
