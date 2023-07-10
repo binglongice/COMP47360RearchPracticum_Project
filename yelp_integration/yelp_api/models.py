c6329f82139c75967dd879084ee8846a4b8c56bd
larkincol <123472310+larkincol@users.noreply.github.com>
2023-07-10 16:15:41 +0100

    got business ids

--- yelp_integration/yelp_api/models.py
+++ yelp_integration/yelp_api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Cafe(models.Model):
-    id = models.CharField(primary_key=True, max_length=255)
+    id = models.CharField(primary_key=True,max_length=255)
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     #location = models.CharField(max_length=255)
