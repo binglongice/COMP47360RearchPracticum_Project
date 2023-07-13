6335ec8cdaae0582c728dcc0aced77b9a3d7afad
larkincol <123472310+larkincol@users.noreply.github.com>
2023-07-13 18:24:56 +0100

    added pw to gitignore

--- yelp_integration/yelp_integration/settings.py
+++ yelp_integration/yelp_integration/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "cafes_manhattan",
-        "USER": "cormacegan",
+        "USER": "postgres",
         "PASSWORD": "",
         "HOST": "localhost",
         "PORT": "5432",
