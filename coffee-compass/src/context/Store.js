5cf5e228ec197974f09ee851a1f2735de7678ce0
Cormac-E <107369515+Cormac-E@users.noreply.github.com>
2023-07-16 13:13:50 +0100

    normalised with model_number included

--- coffee-compass/src/context/Store.js
+++ coffee-compass/src/context/Store.js
@@ -55,13 +55,15 @@ function Store({ children, selectedCafeId }) {
   }, [selectedCafeId]);
 
   //new useEffect for model predictions - CURRENTLY HARDCODED - seems to be logging twice
+  //Hour, Day, Month, Week_of_Month 
+  //0-23, 0-6, 1-12, 0-4
   useEffect(() => {
     fetch('http://localhost:8000/yelp_api/pickle_views/model-output/11/3/9/2/')
       .then(response => response.json())
       .then(data => {
         // Access the predictions data here
         console.log('new state for predictions', data.predictions);
-        // setPicklePredictions(data.predictions)
+        setPicklePredictions(data.predictions)
         // console.log('new state for predictions', picklePredictions)
         // Use the data to generate the heatmap or perform other operations
         // ...
@@ -73,7 +75,7 @@ function Store({ children, selectedCafeId }) {
   
 
   return (
-    <ApiContext.Provider value={[data, setData, reviews, setReviews]}>
+    <ApiContext.Provider value={[data, setData, reviews, setReviews, picklePredictions,setPicklePredictions]}>
       {children}
     </ApiContext.Provider>
   );
--- yelp_integration/yelp_api/pickle_views.py
+++ yelp_integration/yelp_api/pickle_views.py
@@ -15,34 +15,35 @@ def model_output_api(request, hour, day, month, week_of_month):
 
     # Define the model numbers you want to include
     model_numbers = [4, 12, 13, 24, 41, 42, 43, 45, 48, 50, 68, 74, 75, 79, 87, 88, 90, 100, 107, 113,
-                114, 116, 120, 125, 127, 128, 137, 140, 141, 142, 143, 144, 148, 151, 152, 153,
-                158, 161, 162, 163, 164, 166, 170, 186, 194, 202, 209, 211, 224, 229, 230, 231,
-                232, 233, 234, 236, 237, 238, 239, 243, 244, 246, 249, 261, 262, 263]
+                     114, 116, 120, 125, 127, 128, 137, 140, 141, 142, 143, 144, 148, 151, 152, 153,
+                     158, 161, 162, 163, 164, 166, 170, 186, 194, 202, 209, 211, 224, 229, 230, 231,
+                     232, 233, 234, 236, 237, 238, 239, 243, 244, 246, 249, 261, 262, 263]
 
     # Prepare the inputs
     inputs = [hour, day, month, week_of_month]
 
     # Load pickle files and make predictions for each model
-    predictions = []
+    predictions = {}
+    all_predictions = []
     for model_number in model_numbers:
         pickle_file = os.path.join('pickle_models', f'model_{model_number}.pkl')
         with open(pickle_file, 'rb') as f:
             model = pickle.load(f)
 
         prediction = model.predict([inputs])
-        predictions.append(prediction)
+        all_predictions.extend(prediction)
+
+        predictions[f'model_{model_number}'] = prediction.tolist()
+
+    # Compute min and max across all predictions
+    prediction_min = min(all_predictions)
+    prediction_max = max(all_predictions)
 
     # Normalize predictions using min-max scaling
-    predictions = np.array(predictions)
-    prediction_min = np.min(predictions)
-    prediction_max = np.max(predictions)
-    normalized_predictions = (predictions - prediction_min) / (prediction_max - prediction_min)
-
-    # Convert normalized predictions to list
-    normalized_predictions = normalized_predictions.tolist()
+    for model_number in model_numbers:
+        prediction = np.array(predictions[f'model_{model_number}'])
+        normalized_prediction = (prediction - prediction_min) / (prediction_max - prediction_min)
+        predictions[f'model_{model_number}'] = normalized_prediction.tolist()
 
     # Return the predictions as a JSON response
-    response_data = {
-        'predictions': normalized_predictions
-    }
-    return JsonResponse(response_data)
+    return JsonResponse(predictions)
