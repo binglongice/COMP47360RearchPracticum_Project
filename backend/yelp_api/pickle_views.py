e41ee46fb5687c92ead7334631babd5ed20bcc99
larkincol <123472310+larkincol@users.noreply.github.com>
2023-07-26 13:30:01 +0100

    added two APIs

--- backend/yelp_api/pickle_views.py
+++ backend/yelp_api/pickle_views.py
@@ -8,6 +8,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import redis
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from yelp_api.models import AggregatedPredictions, MonthlyPredictions
+
 
 REDIS_HOST = '127.0.0.1'
 REDIS_PORT = '6379'
@@ -101,4 +104,171 @@ def model_output_api(request, hour, day, month, week_of_year):
 
     # Return the predictions as a JSON response
     return JsonResponse(all_model_predictions)
+
+
+
+
+@api_view(['GET'])
+def weekly_aggregation_api(request, month, week_of_year):
+    # Retrieve data from the AggregatedPredictions model based on the provided parameters
+    predictions_data = AggregatedPredictions.objects.filter(
+        month=month,
+        week_of_year=week_of_year,
+    )
+
+    # Check if any predictions are found, return a 404 response if not found
+    if not predictions_data.exists():
+        return Response({'error': 'Data not found.'}, status=404)
+
+    # Calculate the global min and max values for normalization
+    all_predictions = [prediction.average_prediction for prediction in predictions_data]
+
+    prediction_min = min(all_predictions)
+    prediction_max = max(all_predictions)
+
+    # Prepare the output data
+    all_model_predictions = []
+
+    # Prepare the inputs
+    inputs = [month, week_of_year]
+
+    try:
+        # Attempt to connect to Redis
+        redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+
+        # Generate the Redis key using the input parameters
+        redis_key = f"model_predictions:{month}-{week_of_year}"
+
+        # Check if the predictions are already cached in Redis
+        cached_predictions = redis_client.get(redis_key)
+
+        if cached_predictions is not None:
+            # If the predictions are already cached, use the cached data
+            all_model_predictions = json.loads(cached_predictions)
+            print("Cache was used for predictions.")
+        else:
+            # If the predictions are not cached, unpickle the models and calculate predictions
+            for prediction in predictions_data:
+                #prediction_min = min(prediction.average_prediction for prediction in predictions_data)
+                #prediction_max = max(prediction.average_prediction for prediction in predictions_data)
+                normalized_prediction = (prediction.average_prediction - prediction_min) / (prediction_max - prediction_min)
+                all_model_predictions.append({
+                    'location_id': prediction.location_id,
+                    'day': prediction.day,
+                    'month': prediction.month,
+                    'week_of_year': prediction.week_of_year,
+                    'normalized_prediction': normalized_prediction,
+                })
+
+            # Store all the predictions as one large JSON object in Redis
+            redis_client.set(redis_key, json.dumps(all_model_predictions))
+            print("Cache was not used for predictions. Unpickled models instead")
+
+    except redis.ConnectionError:
+        # Handle Redis connection error gracefully
+        print("Error: Unable to connect to the Redis cache for predictions.")
+        # If Redis connection fails, directly calculate the predictions without using Redis
+        for prediction in predictions_data:
+            #prediction_min = min(prediction.average_prediction for prediction in predictions_data)
+            #prediction_max = max(prediction.average_prediction for prediction in predictions_data)
+            normalized_prediction = (prediction.average_prediction - prediction_min) / (prediction_max - prediction_min)
+            all_model_predictions.append({
+                'location_id': prediction.location_id,
+                'day': prediction.day,
+                'month': prediction.month,
+                'week_of_year': prediction.week_of_year,
+                'normalized_prediction': normalized_prediction,
+            })
+
+        print("Normalization has occurred for predictions. Unpickled models instead")
+
+    # Return the predictions as a JSON response
+    return JsonResponse(all_model_predictions, safe=False)
+
+
+
+@api_view(['GET'])
+
+def monthly_aggregation_api(request):
+    # Retrieve data from the AggregatedPredictions model based on the provided parameters
+    predictions_data = MonthlyPredictions.objects.all()
+    
+    # Check if any predictions are found, return a 404 response if not found
+    if not predictions_data.exists():
+        return Response({'error': 'Data not found.'}, status=404)
+    
+    # Calculate the global min and max values for normalization
+    all_predictions = [prediction.monthly_prediction for prediction in predictions_data]
+
+    prediction_min = min(all_predictions)
+    prediction_max = max(all_predictions)
+
+    # Prepare the output data
+    all_model_predictions = []
+
+    try:
+        # Attempt to connect to Redis
+        redis_client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+
+        # Generate the Redis key using the input parameters
+        redis_key = f"model_predictions:{MonthlyPredictions.objects.all()}"
+
+        # Check if the predictions are already cached in Redis
+        cached_predictions = redis_client.get(redis_key)
+
+        if cached_predictions is not None:
+            # If the predictions are already cached, use the cached data
+            all_model_predictions = json.loads(cached_predictions)
+            print("Cache was used for predictions.")
+        else:
+            # If the predictions are not cached, unpickle the models and calculate predictions
+            for prediction in predictions_data:
+                #prediction_min = min(prediction.average_prediction for prediction in predictions_data)
+                #prediction_max = max(prediction.average_prediction for prediction in predictions_data)
+                normalized_prediction = (prediction.monthly_prediction - prediction_min) / (prediction_max - prediction_min)
+                all_model_predictions.append({
+                    'location_id': prediction.location_id,
+                    'month': prediction.month,
+                    'normalized_prediction': normalized_prediction,
+                })
+
+            # Store all the predictions as one large JSON object in Redis
+            redis_client.set(redis_key, json.dumps(all_model_predictions))
+            print("Cache was not used for predictions. Unpickled models instead")
+
+    except redis.ConnectionError:
+        # Handle Redis connection error gracefully
+        print("Error: Unable to connect to the Redis cache for predictions.")
+        # If Redis connection fails, directly calculate the predictions without using Redis
+        for prediction in predictions_data:
+            #prediction_min = min(prediction.average_prediction for prediction in predictions_data)
+            #prediction_max = max(prediction.average_prediction for prediction in predictions_data)
+            normalized_prediction = (prediction.monthly_prediction - prediction_min) / (prediction_max - prediction_min)
+            all_model_predictions.append({
+                'location_id': prediction.location_id,
+                'month': prediction.month,
+                'normalized_prediction': normalized_prediction,
+            })
+
+        print("Normalization has occurred for predictions. Unpickled models instead")
+
+    # Return the predictions as a JSON response
+    return JsonResponse(all_model_predictions, safe=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- backend/yelp_api/urls.py
+++ backend/yelp_api/urls.py
@@ -8,7 +8,8 @@
 from django.urls import path
 from .views import cafes_api
 from . views import predictions_api, review_api
-from .pickle_views import model_output_api
+from .pickle_views import model_output_api, weekly_aggregation_api, monthly_aggregation_api
+
 #this needs to be modified in dev
 
 urlpatterns = [
@@ -16,4 +17,8 @@ urlpatterns = [
     path('api/reviews/<str:id>/', review_api),
     path('api/predictions/<str:location>/', predictions_api, name='predictions_api'),
     path('pickle_views/model-output/<int:hour>/<int:day>/<int:month>/<int:week_of_year>/', model_output_api, name='model-output-api'),
+    path('pickle_views/weekly/<int:month>/<int:week_of_year>/', weekly_aggregation_api, name='weekly-aggregation-api'),
+    path('pickle_views/monthly/', monthly_aggregation_api, name='monthly-aggregation-api'),
+
+
 ]
