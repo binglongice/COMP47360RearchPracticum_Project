68a7b599b6a9674532adef9b9b9847f80e3c41ad
nicholashegarty <nicholasahegartynah@gmail.com>
2023-08-04 12:20:16 +0100

    Date and Takeout button styled but func not working

--- coffee-compass/src/components/TakeOutBox.js
+++ coffee-compass/src/components/TakeOutBox.js
@@ -3,105 +3,100 @@ import React, { useState, useEffect, useContext } from 'react';
 import mapboxgl from 'mapbox-gl';
 import MapContext from '../context/MapContext';
 import customMarkerImage from '../coffee.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWalking, faBicycle, faCar } from '@fortawesome/free-solid-svg-icons';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWF4MTczOCIsImEiOiJjbGoybXdvc3QxZGZxM2xzOTRpdGtqbmMzIn0.ZLAd2HM1pH6fm49LnVzK5g';
-// we might use a different mapbox api key so we don't go over 
 
-// Import your custom marker image
-
-//https://docs.mapbox.com/help/tutorials/get-started-isochrone-api/
-
-
-const TakeOutBox = ({setProfile, setMinutes, setTakeoutLat, setTakeoutLng}) => {
+const TakeOutBox = ({ setProfile, setMinutes, setTakeoutLat, setTakeoutLng }) => {
     const map = useContext(MapContext); // Get the map instance
-    
-    const handleProfileChange = (e) => {
-        setProfile(e.target.value);
-    };
 
-    const handleDurationChange = (e) => {
-        setMinutes(e.target.value);
-    };
+    const [modeIndex, setModeIndex] = useState(0);
+    const [durationIndex, setDurationIndex] = useState(0);
+    const [mapClickActive, setMapClickActive] = useState(false);
+
+    const modeOptions = [
+        { icon: faWalking, value: 'walking' },
+        { icon: faBicycle, value: 'cycling' },
+        { icon: faCar, value: 'driving' },
+    ];
+
+    const durationOptions = ['10m', '20m', '30m'];
 
     useEffect(() => {
-        return () => {
-            // Cleanup on unmount: remove the event listener
-            if (map) map.off('click', onMapClick);
-        };
-    }, []);
+        setProfile(modeOptions[modeIndex].value);
+    }, [modeIndex]); // Set mode whenever modeIndex changes
 
-    const onMapClick = (e) => {
-        const marker = document.querySelector('.mapboxgl-marker');
-        if (marker) {
-            map.on('click', onMapClick); 
-            marker.remove();  
+    useEffect(() => {
+        setMinutes(durationOptions[durationIndex]);
+    }, [durationIndex]); // Set duration whenever durationIndex changes
+
+    useEffect(() => {
+        if (mapClickActive) {
+            const onMapClick = (e) => {
+                const marker = document.querySelector('.mapboxgl-marker');
+                if (marker) {
+                    marker.remove();
+                }
+
+                const { lng, lat } = e.lngLat;
+
+                // Create a new marker and add it to the map
+                const customMarkerElement = document.createElement('img');
+                customMarkerElement.src = customMarkerImage;
+                customMarkerElement.classList.add('custom-marker');
+
+                new mapboxgl.Marker({ element: customMarkerElement })
+                    .setLngLat([lng, lat])
+                    .addTo(map);
+
+                setTakeoutLat(lat);
+                setTakeoutLng(lng);
+
+                setMapClickActive(false); // Deactivate map click after marker placement
+            };
+
+            map.on('click', onMapClick);
+
+            return () => {
+                // Cleanup on unmount: remove the event listener
+                map.off('click', onMapClick);
+            };
         }
+    }, [mapClickActive]); // Run this effect whenever mapClickActive changes
 
+    const handleModeChange = () => {
+        setModeIndex((prevIndex) => (prevIndex + 1) % modeOptions.length);
+    };
 
-
-        const { lng, lat } = e.lngLat;
-
-
-        // Create a new marker and add it to the map
-        const customMarkerElement = document.createElement('img');
-        customMarkerElement.src = customMarkerImage;
-        customMarkerElement.classList.add('custom-marker');
-    
-        new mapboxgl.Marker({ element: customMarkerElement }) // Use the custom marker element
-            .setLngLat([lng, lat])
-            .addTo(map);
-            console.log("takeout coords", lng,lat)
-        //check to see if a marker exists, if it does, remove it
-        setTakeoutLat(lat);
-        setTakeoutLng(lng);
-
-        // After placing the marker, remove the event listener
-        map.off('click', onMapClick);
+    const handleDurationChange = () => {
+        setDurationIndex((prevIndex) => (prevIndex + 1) % durationOptions.length);
     };
 
     const createMarker = () => {
-        
-        if (map) map.on('click', onMapClick);
-        
+        setMapClickActive(true); // Activate map click for marker placement
     };
-    
+
     return (
         <div className='takeout-box-container'>
             <form id='params'>
                 <h4 className='mode-selection-header'>Choose a travel mode:</h4>
                 <div className='mode-selection-options'>
-                    <label className='mode-option'>
-                        <input name='profile' type='radio' value='walking' onChange={handleProfileChange} />
-                        <div className='mode-label'>Walking</div>
-                    </label>
-                    <label className='mode-option'>
-                        <input name='profile' type='radio' value='cycling' onChange={handleProfileChange} />
-                        <div className='mode-label'>Cycling</div>
-                    </label>
-                    <label className='mode-option'>
-                        <input name='profile' type='radio' value='driving' onChange={handleProfileChange}/>
-                        <div className='mode-label'>Driving</div>
-                    </label>
+                    <button className='mode-option' onClick={handleModeChange}>
+                        <FontAwesomeIcon icon={modeOptions[modeIndex].icon} />
+                    </button>
                 </div>
                 <h4 className='duration-selection-header'>Choose a maximum duration:</h4>
                 <div className='duration-selection-options'>
-                    <label className='duration-option'>
-                        <input name='duration' type='radio' value='10' onChange={handleDurationChange} />
-                        <div className='duration-label'>10 min</div>
-                    </label>
-                    <label className='duration-option'>
-                        <input name='duration' type='radio' value='20' onChange={handleDurationChange}/>
-                        <div className='duration-label'>20 min</div>
-                    </label>
-                    <label className='duration-option'>
-                        <input name='duration' type='radio' value='30' onChange={handleDurationChange}/>
-                        <div className='duration-label'>30 min</div>
-                    </label>
+                    <button className='duration-option' onClick={handleDurationChange}>
+                        {durationOptions[durationIndex]}
+                    </button>
                 </div>
-                <div className="button-container">
-                    <button className="submit-button" type="button" onClick={createMarker}>Create a Marker</button>
+                <div className='button-container'>
+                    <button className='submit-button' type='button' onClick={createMarker}>
+                        Create a Marker
+                    </button>
                 </div>
-
             </form>
         </div>
     );
